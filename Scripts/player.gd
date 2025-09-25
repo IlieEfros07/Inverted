8c9345456fd840e63069546ad77c1793fcf2d9a2
IlieEfros07 <ilie159753@gmail.com>
2025-09-25 19:53:39 +0300

    Added a way to flip the gravity

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,23 +3,57 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -200.0
+var gravityDir = 1
+var gravity = 900
+
+@onready var animatedSprite2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		
+		velocity.y += gravity * gravityDir * delta
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("up"):
+		gravityDir *= -1
+		velocity.y = gravity * gravityDir * delta
+	if Input.is_action_just_pressed("down"):
+		gravity *= -1
+		velocity.y = gravity * gravityDir * delta
+		
+
+
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
+	
+	if direction > 0:
+		animatedSprite2d.flip_h=false
+	elif direction < 0:
+		animatedSprite2d.flip_h=true 
+	
+	if is_on_floor():
+		if direction == 0:
+			animatedSprite2d.play("Idle")
+		else:
+			animatedSprite2d.play("Walk")
+	else:
+		animatedSprite2d.play("Flip")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+
+	
